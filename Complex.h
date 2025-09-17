1e2cacb58d5326157ed3d0a4abd929f13c858be9
wuyntram04 <hothiquynhtram2004@gmail.com>
2025-09-16 21:54:25 -0700

    Update Complex.h

--- Complex.h
+++ Complex.h
@@ -1,106 +1,71 @@
 #pragma once
 #include<iostream>
-#include"input.h"
+#include "input.h"
 using namespace std;
 
 class Complex
 {
-
+private:
+	double realNum;
+	double iNum;
 public:
 
-
-	void menuA()
-	{
-		do
-		{
-			system("cls");
-
-			cout << "\n\tA> A Complex Number";
-			cout << "\n\t" << string(80, char(205));
-			cout << "\n\t\t1. Enter the real number";
-			cout << "\n\t\t2. Enter the imaginary number";
-			cout << "\n\t\t3. Display the complex number";
-			cout << "\n\t\t4. Negate the complex number";
-			cout << "\n\t\t5. Add (+) the complex number with a constant";
-			cout << "\n\t\t6. Subtract (-) the complex number with a constant";
-			cout << "\n\t\t7. Multiply (*) the complex number with a constant";
-			cout << "\n\t\t8. Divide (/) the complex number with a constant";
-			cout << "\n\t" << string(80, char(196));
-			cout << "\n\t\t0. return";
-			cout << "\n\t" << string(80, char(205));
-
-			switch (toupper(inputInteger("\n\t\tOption: ", 0, 8)))
-			{
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 6:
-				break;
-			case 7:
-				break;
-			case 8:
-				break;
-			case 0: return;
-			default:
-				cout << "\n\tInvalid input";
-				break;
-
-			}
-
-			cout << "\n\n\t";
-			system("pause");
-
-		} while (true);
+	Complex() {
+		realNum = 0.0;
+		iNum = 0.0;
 	}
 
-	void menuB()
+	void setRealNum(double real)
 	{
-		do
-		{
-			system("cls");
-
-			cout << "\n\tB> Multiple Complex Numbers";
-			cout << "\n\t" << string(90, char(205));
-			cout << "\n\t\t1. Enter complex number C1";
-			cout << "\n\t\t2. Enter complex number C2";
-			cout << "\n\t\t3. Verify condition operators (== and !=) of C1 and C2";
-			cout << "\n\t\t4. Evaluate arithmatic operators (+, - , * and /) of C1 and C2";
-			cout << "\n\t\t5. Evaluate steps in (3 * (C1 + C2) / 7) / (C2 - C1 / 9) != (1.07109 + 0.120832i) ?";
-			cout << "\n\t" << string(90, char(196));
-			cout << "\n\t\t0. return";
-			cout << "\n\t" << string(90, char(205));
-
-			switch (toupper(inputInteger("\n\t\tOption: ", 0, 8)))
-			{
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			case 5:
-				break;
-			case 0: return;
-			default:
-				cout << "\n\tInvalid input";
-				break;
-
-			}
-
-			cout << "\n\n\t";
-			system("pause");
-
-		} while (true);
+		realNum = real;
 	}
+
+	double getRealNum() const
+	{
+		return realNum;
+	}
+
+	void setiNum(double i)
+	{
+		iNum = i;
+	}
+
+	double getiNum() const
+	{
+		return iNum;
+	}
+
+	void showFomular() const
+	{
+		cout << "\n\tComplex Number C1 = ";
+
+		if (realNum == 0 && iNum == 0) {
+			cout << "0";
+		}
+		else if (realNum != 0 && iNum == 0) {
+			cout << realNum;
+		}
+		else if (realNum == 0 && iNum != 0) {
+			cout << iNum << "i";
+		}
+		else
+		{
+			cout << realNum;
+			if (iNum > 0)
+			{
+				cout << " + " << iNum << "i";
+			}
+			else
+			{
+				cout << iNum << "i";
+			}
+		}
+		cout << "\n";
+	}
+
+	
 };
+
+
 
 
